5ee76c79c600711a7432812453d87c260dfd6d31
Wang Yu Teng <wyuteng123@gmail.com>
2023-03-14 15:45:30 +0800

    Added SignInForm and SignUpForm components
    - Links to each other
    - New package dependencies installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import {Routes, Route} from 'react-router-dom';
+import SignInComponent from './components/SignInForm';
+import SignUpComponent from './components/SignUpForm';
+
 export default function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <CssBaseline>
+      <Routes>
+        <Route path = '/Login' element={<SignInComponent />}/>
+        <Route path = '/Register' element={<SignUpComponent />}/>
+      </Routes>
+    </CssBaseline>
   )
 }
 
