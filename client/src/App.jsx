02a890e9331f7d8804ba2d306d1fb25c60012f5d
Wang Yu Teng <wyuteng123@gmail.com>
2023-03-15 23:47:04 +0800

    feat(NavBar): added NavBar to Login and Register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import {Routes, Route} from 'react-router-dom';
-import SignInComponent from './components/SignInForm';
-import SignUpComponent from './components/SignUpForm';
 import IndexPage from './pages/IndexPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 
 export default function App() {
-
   return (
     <CssBaseline>
       <Routes>
         <Route index element={<IndexPage />} />
-        <Route path = '/Login' element={<SignInComponent />}/>
-        <Route path = '/Register' element={<SignUpComponent />} />
+        <Route path = '/Login' element={<LoginPage />}/>
+        <Route path = '/Register' element={<RegisterPage />} />
       </Routes>
     </CssBaseline>
   )
 }
+
+
 
