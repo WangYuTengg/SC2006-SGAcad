0773e05a342c1d5bca587d1aab55ad646b2bd959
WangYuTengg <wyuteng123@gmail.com>
2023-03-22 15:12:24 +0800

    feat(theme): added theme to frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,32 @@
-import CssBaseline from '@mui/material/CssBaseline';
+import { CssBaseline, ThemeProvider} from '@mui/material';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import Layout from './components/Layout';
+import { useMemo } from "react";
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from 'theme';
+import { useSelector } from 'react-redux';
 
 export default function App() {
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
-    <CssBaseline>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />} >
-            <Route index element={<IndexPage />} />
-            <Route path = '/Login' element={<LoginPage />}/>
-            <Route path = '/Register' element={<RegisterPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </CssBaseline>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <Routes>
+            <Route path="/" element={<Layout />} >
+              <Route index element={<IndexPage />} />
+              <Route path = '/Login' element={<LoginPage />}/>
+              <Route path = '/Register' element={<RegisterPage />} />
+            </Route>
+          </Routes>
+        </CssBaseline>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
