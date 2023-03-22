a1213ed16fefda27552562bf5cbd7a2398255c39
WangYuTengg <wyuteng123@gmail.com>
2023-03-22 16:49:52 +0800

    fix(RegisterPage): removed RegisterPage
    - combined with LoginPage

--- client/src/pages/RegisterPage.jsx
+++
@@ -1,9 +0,0 @@
-import SignUpComponent from "../components/SignUpForm";
-
-export default function RegisterPage(){
-    return(
-        <div>
-            <SignUpComponent />
-        </div>
-    )
-}
