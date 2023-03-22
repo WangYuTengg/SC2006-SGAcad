4ea05d12e094df722a4b1cdb2b86c8934d93dd0a
appleworm34 <xavieryeo2@gmail.com>
2023-03-22 21:26:18 +0800

    Add google maps to StudySpotPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import StudySpotPage from './pages/StudySpotPage';
 import Layout from './components/Layout';
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
           <Route index element={<IndexPage />} />
           <Route path = '/Login' element={<LoginPage />}/>
           <Route path = '/Register' element={<RegisterPage />} />
+          <Route path = '/studyspots/:spotId' element={<StudySpotPage />} />
         </Route>
       </Routes>
     </CssBaseline>
--- client/src/pages/StudySpotPage.jsx
+++ client/src/pages/StudySpotPage.jsx
@@ -1,11 +1,45 @@
 import { Box, useMediaQuery } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import Map from "../components/Map";
 
 const StudySpotPage = () => {
-    return <div>StudySpotPage</div>
+    const [spot, setSpot] = useState(null); // setSpot is a function used to update spot
+    const { spotId } = useParams();
+    //const token = useSelector((state) => state.token);
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+    const getSpot = async () => {
+        const response = await fetch(`http://localhost:3001/studyspots/${spotId}`, {
+            method: "GET",
+            //headers: { Authorization: `Bearer ${token}` },
+        });
+        const data = await response.json();
+        setSpot(data);
+    }
+
+    useEffect(() => {
+        getSpot();
+    }, [])
+
+    if(!spot) return null;
+    //console.log(spot.location.coordinates);
+
+    return (
+        <Box>
+            <Box
+                width="100%"
+                padding="2rem 6%"
+                display={isNonMobileScreens ? "flex" : "block"}
+                gap="2rem"
+                justifyContent="center"
+            >
+                <Map spotLocation={spot.location} />
+            </Box>
+        </Box>
+    )
 }
 
 export default StudySpotPage;
