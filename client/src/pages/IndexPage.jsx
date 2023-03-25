0e57f1749e44bec3a1c8c21c465b58d5a9aa5d44
WangYuTengg <wyuteng123@gmail.com>
2023-03-25 18:11:20 +0800

    feat(IndexPage): get all spots

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -3,10 +3,12 @@ import { useEffect, useState } from "react";
 import Map from "../components/Map";
 
 const IndexPage = () => {
+  const [spots, setSpots] = useState(null);
   const [defaultSpot, setDefaultSpot] = useState(null);
-  const defaultSpotId = "641408f9b4b8c89c10e2f0e4";
+  const defaultSpotId = "641ec083982851ff40de0aa3";
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
+  // initial spot on map
   const getDefaultSpot = async () => {
     const response = await fetch(
       `http://localhost:3001/studyspots/${defaultSpotId}`,
@@ -18,11 +20,24 @@ const IndexPage = () => {
     setDefaultSpot(data);
   };
 
+  // get all spots
+  const getSpots = async () => {
+    const response = await fetch(`http://localhost:3001/studyspots/`, {
+      method: "GET",
+    });
+    const data = await response.json();
+    setSpots(data);
+  };
+
+  // initialise
   useEffect(() => {
     getDefaultSpot();
+    getSpots();
   }, []);
 
   if (!defaultSpot) return null;
+  if (!spots) return null;
+
   return (
     <Box>
       <Box
