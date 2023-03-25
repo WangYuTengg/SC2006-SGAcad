f1c488a2257df993ed34015085220ad951036af2
WangYuTengg <wyuteng123@gmail.com>
2023-03-25 21:53:45 +0800

    feat(IndexPage): initial UI ver

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,12 +1,25 @@
-import { Box, useMediaQuery } from "@mui/material";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useEffect, useState } from "react";
 import Map from "../components/Map";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import { useNavigate } from "react-router-dom";
 
 const IndexPage = () => {
   const [spots, setSpots] = useState(null);
   const [defaultSpot, setDefaultSpot] = useState(null);
   const defaultSpotId = "641ec083982851ff40de0aa3";
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const navigate = useNavigate();
+
+  // THEME
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
 
   // initial spot on map
   const getDefaultSpot = async () => {
@@ -39,17 +52,77 @@ const IndexPage = () => {
   if (!spots) return null;
 
   return (
-    <Box>
-      <Box
-        width="100%"
-        padding="2rem 6%"
-        display={isNonMobileScreens ? "flex" : "block"}
-        gap="2rem"
-        justifyContent="center"
-      >
-        <Map spotLocation={defaultSpot.location} width="600px" height="550px" />
-      </Box>
-    </Box>
+    <>
+      {isNonMobileScreens && (
+        <Box>
+          <Box
+            padding="2rem 1rem"
+            display="flex"
+            flexDirection="column"
+            border="1px solid white"
+            justifyContent="center"
+            alignItems="center"
+            textAlign="center"
+          >
+            <Typography fontWeight="500" fontSize="2rem" color="primary">
+              Welcome to SG Acad!
+            </Typography>
+            <Typography paddingBottom="8px" fontSize="1.1rem">
+              Find your perfect study spot here at our website :). Take a look
+              around!
+            </Typography>
+            <Map
+              spotLocation={defaultSpot.location}
+              width="80%"
+              height="500px"
+            />
+          </Box>
+          <Box
+            padding="2rem 10%"
+            width="100%"
+            border="1px solid white"
+            display="flex"
+            flexDirection="column"
+          >
+            <Typography fontWeight="500" fontSize="2rem" color="primary">
+              Recommended Spots
+            </Typography>
+            <ImageList>
+              {spots.slice(0, 4).map((spot) => (
+                <Box>
+                  <Typography
+                    fontWeight="bold"
+                    fontSize="1.3rem"
+                    onClick={() => navigate(`/studyspots/${spot._id}`)}
+                    sx={{
+                      "&:hover": {
+                        color: primaryLight,
+                        cursor: "pointer",
+                      },
+                    }}
+                  >
+                    {spot.name}
+                  </Typography>
+                  <ImageListItem>
+                    <img
+                      src={`${spot.picturePath}`}
+                      alt={spot.name}
+                      loading="lazy"
+                    />
+                    <ImageListItemBar
+                      title={
+                        spot.location.address + ",  " + spot.location.postal
+                      }
+                      position="below"
+                    />
+                  </ImageListItem>
+                </Box>
+              ))}
+            </ImageList>
+          </Box>
+        </Box>
+      )}
+    </>
   );
 };
 
--- client/src/pages/StudySpotPage.jsx
+++ client/src/pages/StudySpotPage.jsx
@@ -1,45 +1,41 @@
-import { Box, useMediaQuery } from "@mui/material";
+import { Box, Typography, useMediaQuery } from "@mui/material";
 import { useEffect, useState } from "react";
-// import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import NavBar from "../components/NavBar";
 import Map from "../components/Map";
 
 const StudySpotPage = () => {
-    const [spot, setSpot] = useState(null); // setSpot is a function used to update spot
-    const { spotId } = useParams();
-    //const token = useSelector((state) => state.token);
-    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const [spot, setSpot] = useState(null);
+  const { spotId } = useParams();
+  console.log(spotId);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
-    const getSpot = async () => {
-        const response = await fetch(`http://localhost:3001/studyspots/${spotId}`, {
-            method: "GET",
-            //headers: { Authorization: `Bearer ${token}` },
-        });
-        const data = await response.json();
-        setSpot(data);
-    }
+  const getSpot = async () => {
+    const response = await fetch(`http://localhost:3001/studyspots/${spotId}`, {
+      method: "GET",
+    });
+    const data = await response.json();
+    setSpot(data);
+  };
 
-    useEffect(() => {
-        getSpot();
-    }, [])
+  useEffect(() => {
+    getSpot();
+  }, []);
 
-    if(!spot) return null;
-    //console.log(spot.location.coordinates);
-
-    return (
-        <Box>
-            <Box
-                width="100%"
-                padding="2rem 6%"
-                display={isNonMobileScreens ? "flex" : "block"}
-                gap="2rem"
-                justifyContent="center"
-            >
-                <Map spotLocation={spot.location} />
-            </Box>
-        </Box>
-    )
-}
+  if (!spot) return null;
+  return (
+    <Box display="flex">
+      <Typography>{spot.name}</Typography>
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="2rem"
+        justifyContent="center"
+      >
+        <Map spotLocation={spot.location} width="50vh" height="50vh" />
+      </Box>
+    </Box>
+  );
+};
 
 export default StudySpotPage;
