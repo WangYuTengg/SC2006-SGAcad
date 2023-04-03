48125e5c65d5748098fb3d8701fbdf4af291ec99
Wang Yu Teng <wyuteng123@gmail.com>
2023-04-03 19:43:22 +0800

    chore(state): reformat + documentation

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,34 +1,60 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    mode: "light",
-    user: null,
-    token: null,
-    //favoriteSpots: [],
+  mode: "light",
+  user: null,
+  token: null,
 };
 
+/**
+ * Redux slice for authentication state.
+ * @constant
+ */
 export const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        setMode: (state) => {state.mode = state.mode === "light" ? "dark" : "light";},
-        setLogin: (state, action) => {
-            state.user = action.payload.user;
-            state.token = action.payload.token;
-        },
-        setLogout: (state) => {
-            state.user = null;
-            state.token = null;
-        },
-        setFavoriteSpots: (state, action) => {
-            if(state.user) {
-                state.user.favoriteSpots = action.payload.favoriteSpots;
-            } else {
-                console.error("No favourite spots");
-            }
-        }
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    /**
+     * Toggle the mode between light and dark.
+     * @param {Object} state - The state object.
+     */
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    
+    /**
+     * Set the login state with user and token.
+     * @param {Object} state - The state object.
+     * @param {Object} action - The action object.
+     */
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
 
-export const { setMode, setLogin, setLogout, setFavoriteSpots} = authSlice.actions;
+    /**
+     * Reset the user and token to null.
+     * @param {Object} state - The state object.
+     */
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+
+    /**
+     * Set favorite spots for the current user.
+     * @param {Object} state - The state object.
+     * @param {Object} action - The action object.
+     */
+    setFavoriteSpots: (state, action) => {
+      if (state.user) {
+        state.user.favoriteSpots = action.payload.favoriteSpots;
+      } else {
+        console.error("No favourite spots");
+      }
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setFavoriteSpots } = authSlice.actions;
 export default authSlice.reducer;
